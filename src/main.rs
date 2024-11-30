bb86678f423172d11e67efe444198ad9b3398b4e
梶塚太智 <kajizukataichi@outlook.jp>
2024-11-30 22:32:30 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -35,58 +35,6 @@ fn main() {
     }
 }
 
-fn tokenize(input: String, delimiter: Vec<char>) -> Option<Vec<String>> {
-    let mut tokens: Vec<String> = Vec::new();
-    let mut current_token = String::new();
-    let mut in_parentheses: usize = 0;
-    let mut in_quote = false;
-
-    for c in input.chars() {
-        match c {
-            '(' | '{' | '[' if !in_quote => {
-                current_token.push(c);
-                in_parentheses += 1;
-            }
-            ')' | '}' | ']' if !in_quote => {
-                current_token.push(c);
-                if in_parentheses > 0 {
-                    in_parentheses -= 1;
-                } else {
-                    return None;
-                }
-            }
-            '"' => {
-                in_quote = !in_quote;
-                current_token.push(c);
-            }
-            other => {
-                if delimiter.contains(&other) && !in_quote {
-                    if in_parentheses != 0 {
-                        current_token.push(c);
-                    } else if !current_token.is_empty() {
-                        tokens.push(current_token.clone());
-                        current_token.clear();
-                    }
-                } else {
-                    current_token.push(c);
-                }
-            }
-        }
-    }
-
-    // Syntax error check
-    if in_quote || in_parentheses != 0 {
-        return None;
-    }
-
-    if in_parentheses == 0 && !current_token.is_empty() {
-        tokens.push(current_token.clone());
-        current_token.clear();
-    }
-
-    Some(tokens)
-}
-
 type Scope = BTreeMap<String, Type>;
 type Program = Vec<Statement>;
 #[derive(Debug, Clone)]
@@ -567,4 +515,56 @@ impl Type {
             Type::Null | Type::Function(_, _) => String::new(),
         }
     }
+}
+
+fn tokenize(input: String, delimiter: Vec<char>) -> Option<Vec<String>> {
+    let mut tokens: Vec<String> = Vec::new();
+    let mut current_token = String::new();
+    let mut in_parentheses: usize = 0;
+    let mut in_quote = false;
+
+    for c in input.chars() {
+        match c {
+            '(' | '{' | '[' if !in_quote => {
+                current_token.push(c);
+                in_parentheses += 1;
+            }
+            ')' | '}' | ']' if !in_quote => {
+                current_token.push(c);
+                if in_parentheses > 0 {
+                    in_parentheses -= 1;
+                } else {
+                    return None;
+                }
+            }
+            '"' => {
+                in_quote = !in_quote;
+                current_token.push(c);
+            }
+            other => {
+                if delimiter.contains(&other) && !in_quote {
+                    if in_parentheses != 0 {
+                        current_token.push(c);
+                    } else if !current_token.is_empty() {
+                        tokens.push(current_token.clone());
+                        current_token.clear();
+                    }
+                } else {
+                    current_token.push(c);
+                }
+            }
+        }
+    }
+
+    // Syntax error check
+    if in_quote || in_parentheses != 0 {
+        return None;
+    }
+
+    if in_parentheses == 0 && !current_token.is_empty() {
+        tokens.push(current_token.clone());
+        current_token.clear();
+    }
+
+    Some(tokens)
 }
