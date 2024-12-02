727465b0a62507caceb0aabc190e16c3a1cee4b6
梶塚太智 <kajizukataichi@outlook.jp>
2024-12-02 12:59:52 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ impl Engine {
         let mut program: Program = Vec::new();
         for line in tokenize(source, vec![';'])? {
             let line = line.trim().to_string();
-            if line.is_empty() {
+            if line.is_empty() || line.starts_with("//") {
                 continue;
             }
             program.push(Statement::parse(line.trim().to_string())?);
